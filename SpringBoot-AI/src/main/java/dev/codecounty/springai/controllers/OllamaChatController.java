d695d0cf1e537ec437ceb149ca490400af1119d9
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2026-06-17 22:38:53 +0530

    initial commit

--- SpringBoot-AI/src/main/java/dev/codecounty/springai/controllers/OllamaChatController.java
+++ SpringBoot-AI/src/main/java/dev/codecounty/springai/controllers/OllamaChatController.java
@@ -1,22 +1,27 @@
 package dev.codecounty.springai.controllers;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ai/ollama")
-public class OllamaController {
+public class OllamaChatController {
 
-    private final org.springframework.ai.ollama.OllamaChatModel chatModel;
+    private final ChatClient chatClient;
 
-    public OllamaController(org.springframework.ai.ollama.OllamaChatModel chatModel) {
-        this.chatModel = chatModel;
+
+    public OllamaChatController(@Qualifier("ollamaChatClient") ChatClient ollamaChatClient) {
+        this.chatClient = ollamaChatClient;
     }
 
-    @org.springframework.web.bind.annotation.GetMapping("/generate")
+    @GetMapping("/chat")
     public String generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        return chatModel.call(message);
+        ChatClient.CallResponseSpec responseSpec = chatClient.prompt(message).call();
+        return responseSpec.content();
     }
 
 }
--- SpringBoot-AI/src/main/java/dev/codecounty/springai/controllers/OpenAIChatController.java
+++ SpringBoot-AI/src/main/java/dev/codecounty/springai/controllers/OpenAIChatController.java
@@ -1,23 +1,35 @@
 package dev.codecounty.springai.controllers;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.ChatClient.CallResponseSpec;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ai/ollama")
-public class OllamaChatController {
+@RequestMapping("/ai/openai")
+public class OpenAIChatController {
 
-    private final org.springframework.ai.ollama.OllamaChatModel chatModel;
+    private final ChatClient chatClient;
 
-    public OllamaChatController(org.springframework.ai.ollama.OllamaChatModel chatModel) {
-        this.chatModel = chatModel;
+/*    // By Default, we need ChatClient.Builder instead of ChatClient
+    public OpenAIChatController(ChatClient.Builder chatClientBuilder) {
+
+        this.chatClient = chatClientBuilder.build();
+    }*/
+
+
+    public OpenAIChatController(@Qualifier("openAiChatClient") ChatClient openAiChatClient) {
+        this.chatClient = openAiChatClient;
     }
 
     @GetMapping("/chat")
     public String generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        return chatModel.call(message);
+        CallResponseSpec responseSpec = chatClient.prompt(message).call();
+        String content = responseSpec.content();
+        return content;
     }
 
 }
